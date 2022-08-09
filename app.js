1f1dbc19da878d13df2e9063a9fe0f454c7a0d25
Jhon Cruz IMM <jhonlcp1997>
2022-08-09 10:56:45 -0500

    5:12

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const morganBody = require('morgan-body');
+
 const dbConnect = require('./config/mongo');
+const { loggerStream } = require("./utils/handleLogger");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage"));
+
+morganBody(app, {
+   noColors:true,
+   stream: loggerStream,
+   skip: function(req, res){
+      return res.statusCode < 400
+   }
+})
 
 app.get('/', (req, res)=>{
    res.send("Holas se dice weibss")
